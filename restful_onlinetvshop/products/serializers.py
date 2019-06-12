9cd3a00c33288c2fabfc252e680528223885abaa
Gideon <Gideon@DESKTOP-SRGSFBC>
2019-06-12 15:08:58 +0700

    brand attribute of product in product serializer

--- restful_onlinetvshop/products/serializers.py
+++ restful_onlinetvshop/products/serializers.py
@@ -4,6 +4,8 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField(many=False)
+
     class Meta:
         model = Product
         fields = ('title', 'price', 'quantity', 'description', 'image_url', 'brand')
